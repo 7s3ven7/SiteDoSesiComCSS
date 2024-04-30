8e1d8bbc57176e9efa7ef4303d5a93e3bedb1401
Wesley_G_Rodrigues <125270476+7s3ven7@users.noreply.github.com>
2024-04-30 18:13:32 -0300

    Refazendo1
    Refazendo o Front-end do zero com auxilio do código antigo para melhorar a qualidade

---
+++ Refazendo/Index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+    <head>
+        <link rel="stylesheet" href="CSS/index.css">
+    </head>
+    <body>
+        <div class="box">
+            <div class="texto">Nome do Programa</div>
+            <div class="espaco"></div>
+            <form action="Cadastro.php">
+            <div class="padding"><input class="botao" value="Cadastrar-se" type="submit"></div>
+            </form>
+            <hr>
+            <form action="Logar.php">
+            <div class="padding"><input class="botao" value="Logar-se" type="submit"></div>
+            </form>
+        </div>
+    </body> 
+</html>
