f863347a6a9fbd42dfdce731918daf165c63d362
Matheus Jastrombek Fernandes <130985777+matheus-j-fernandes@users.noreply.github.com>
2024-05-02 07:50:48 -0300

    save
    update do Banco de dados

---
+++ SitedoSesi/BD/dados (3).sql
@@ -0,0 +1,328 @@
+-- phpMyAdmin SQL Dump
+-- version 4.5.4.1
+-- http://www.phpmyadmin.net
+--
+-- Host: localhost
+-- Generation Time: 02-Maio-2024 às 10:44
+-- Versão do servidor: 5.7.11
+-- PHP Version: 5.6.18
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `dados`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `cliente`
+--
+
+CREATE TABLE `cliente` (
+  `CPF` int(11) NOT NULL,
+  `nome` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+  `CEP` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+  `fone` int(11) NOT NULL,
+  `email` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `estoque`
+--
+
+CREATE TABLE `estoque` (
+  `cod_inter` int(11) NOT NULL,
+  `cod_forne` int(11) NOT NULL,
+  `nome_prod` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `quant_prod` float NOT NULL,
+  `anguar` int(11) NOT NULL,
+  `rua` int(11) NOT NULL,
+  `coluna` int(11) NOT NULL,
+  `andar` int(11) NOT NULL,
+  `ap` int(11) NOT NULL,
+  `Kg` float NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `fornecedor`
+--
+
+CREATE TABLE `fornecedor` (
+  `CNPJ` int(11) NOT NULL,
+  `n_empresa` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+  `email` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+  `CEP` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+  `fone` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `nossa_empresa`
+--
+
+CREATE TABLE `nossa_empresa` (
+  `CNPJ_empresa` int(11) NOT NULL,
+  `CEP` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `gerente` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `nota_expedicao`
+--
+
+CREATE TABLE `nota_expedicao` (
+  `n_nota` int(11) NOT NULL,
+  `CEP` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `n_destinatario` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `fone_desti` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `fone_empresa` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `CNPJ_remet` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `CPF_destina` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `placa_veiculo` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `date_emi` date NOT NULL,
+  `CEP_entrega` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `modelo_v` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `eixos` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `PBT` float NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `nota_recebimento`
+--
+
+CREATE TABLE `nota_recebimento` (
+  `n_nota` int(11) NOT NULL,
+  `CEP` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+  `n_destinatario` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+  `fone_forne` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+  `CNPJ_dest` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+  `CNPJ_remet` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+  `placa_veiculo` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+  `data_emi` date NOT NULL,
+  `haver_pedido` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `CEP_entrega` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+  `CPF_motorista` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+  `nome_motoris` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+  `modelo_v` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+  `eixos` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+  `PBT` float NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `produto`
+--
+
+CREATE TABLE `produto` (
+  `cod_inter` int(11) NOT NULL,
+  `cod_fabri` int(11) NOT NULL,
+  `tipo` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `validade` date DEFAULT NULL,
+  `nome` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `marca_produto` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `embalagem` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `und` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `quantidade_und` int(11) NOT NULL,
+  `valor_und` float NOT NULL,
+  `Kg` float NOT NULL,
+  `NCM` int(11) NOT NULL,
+  `CST` int(11) NOT NULL,
+  `CFOP` int(11) NOT NULL,
+  `n_pedido` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `qualitativo`
+--
+
+CREATE TABLE `qualitativo` (
+  `id` int(11) NOT NULL,
+  `container_desgas` varchar(6) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `avari_late_d` varchar(6) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `avari_late_e` varchar(6) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `avari_teto` varchar(6) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `avaria_frente` varchar(6) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `sem_lacre` varchar(6) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `adesivo_avariado` varchar(6) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `excesso_altu` varchar(6) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `excesso_d` varchar(6) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `excesso_e` varchar(6) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `excesso_fron` varchar(6) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `painel_avariado` varchar(6) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `sem_cabo_energia` varchar(6) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `sem_lona` varchar(6) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `canhoto_ass` varchar(6) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `volume_correto` varchar(6) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `atraso` varchar(6) COLLATE utf8mb4_unicode_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `quantitativa`
+--
+
+CREATE TABLE `quantitativa` (
+  `cod_inter` int(11) NOT NULL,
+  `cod_forne` int(11) NOT NULL,
+  `n_empresa` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+  `CNPJ_empre` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+  `modelo_container` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `navio` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `destinatario` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+  `tipo_container` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `lacre` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `lacre_SIF` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `temperatura` decimal(10,0) DEFAULT NULL,
+  `IMO` int(11) DEFAULT NULL,
+  `n_ONU` int(11) DEFAULT NULL,
+  `nome_prod` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+  `quant_prod` float NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `usuario`
+--
+
+CREATE TABLE `usuario` (
+  `id` int(11) NOT NULL,
+  `tipo` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+  `senha` varchar(4) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+  `cod_grupo` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+  `cargo` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+  `cod_prof` varchar(9) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `cliente`
+--
+ALTER TABLE `cliente`
+  ADD PRIMARY KEY (`CPF`);
+
+--
+-- Indexes for table `estoque`
+--
+ALTER TABLE `estoque`
+  ADD PRIMARY KEY (`cod_inter`);
+
+--
+-- Indexes for table `fornecedor`
+--
+ALTER TABLE `fornecedor`
+  ADD PRIMARY KEY (`CNPJ`);
+
+--
+-- Indexes for table `nossa_empresa`
+--
+ALTER TABLE `nossa_empresa`
+  ADD PRIMARY KEY (`CNPJ_empresa`);
+
+--
+-- Indexes for table `nota_expedicao`
+--
+ALTER TABLE `nota_expedicao`
+  ADD PRIMARY KEY (`n_nota`);
+
+--
+-- Indexes for table `nota_recebimento`
+--
+ALTER TABLE `nota_recebimento`
+  ADD PRIMARY KEY (`n_nota`);
+
+--
+-- Indexes for table `produto`
+--
+ALTER TABLE `produto`
+  ADD PRIMARY KEY (`cod_inter`);
+
+--
+-- Indexes for table `qualitativo`
+--
+ALTER TABLE `qualitativo`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `quantitativa`
+--
+ALTER TABLE `quantitativa`
+  ADD PRIMARY KEY (`cod_inter`);
+
+--
+-- Indexes for table `usuario`
+--
+ALTER TABLE `usuario`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `cliente`
+--
+ALTER TABLE `cliente`
+  MODIFY `CPF` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `estoque`
+--
+ALTER TABLE `estoque`
+  MODIFY `cod_inter` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `fornecedor`
+--
+ALTER TABLE `fornecedor`
+  MODIFY `CNPJ` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `nossa_empresa`
+--
+ALTER TABLE `nossa_empresa`
+  MODIFY `CNPJ_empresa` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `nota_recebimento`
+--
+ALTER TABLE `nota_recebimento`
+  MODIFY `n_nota` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `qualitativo`
+--
+ALTER TABLE `qualitativo`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `quantitativa`
+--
+ALTER TABLE `quantitativa`
+  MODIFY `cod_inter` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `usuario`
+--
+ALTER TABLE `usuario`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
