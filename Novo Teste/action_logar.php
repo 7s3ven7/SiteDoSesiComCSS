20914c8be4e2b2422a44a4146daedc7215735741
Wesley_G_Rodrigues <125270476+7s3ven7@users.noreply.github.com>
2024-05-05 13:58:03 -0300

    save

--- Novo Teste/action_logar.php
+++ Novo Teste/action_logar.php
@@ -5,29 +5,28 @@
 session_start();
 
 $hostname = "127.0.0.1";
-$name ="root";
+$name = "root";
 $password = "root";
 $DB = "dados";
-$conexao = new mysqli($hostname,$name,$password,$DB);
-if($conexao -> connect_errno){
-    echo "Failes conection :" .$conexao -> connect_error;
-    exit();
-}
-else{
-$nome = $conexao -> real_escape_string ($_POST["nome_conta"]);
-$senha = $conexao -> real_escape_string ($_POST["senha_conta"]);
-$SQL = "SELECT `id`, `tipo` FROM `usuario` WHERE `tipo` = '".$nome."' AND `senha` = '".$senha."'";
-$resultado = $conexao -> query($SQL);
-if($resultado->num_rows != 0)
-	{
-		$row = $resultado -> fetch_array();
+$conexao = new mysqli($hostname, $name, $password, $DB);
+if ($conexao->connect_errno) {
+	echo "Failes conection :" . $conexao->connect_error;
+	exit();
+} else {
+	$nome = $conexao->real_escape_string($_POST["nome_conta"]);
+	$senha = $conexao->real_escape_string($_POST["senha_conta"]);
+	$SQL = "SELECT `id`, `tipo` FROM `usuario` WHERE `tipo` = '" . $nome . "' AND `senha` = '" . $senha . "'";
+	$resultado = $conexao->query($SQL);
+	if ($resultado->num_rows != 0) {
+		$row = $resultado->fetch_array();
 		$_SESSION['id'] = $row[0];
 		$_SESSION['tipo'] = $row[1];
-		$conexao -> close();
+		$conexao->close();
 		header('Location: tela.html', true, 301);
 		exit();
 	} else {
-	$conexao -> close();
+		$conexao->close();
 		header('Location: index.html', true, 301);
-	}}
+	}
+}
 ?>
