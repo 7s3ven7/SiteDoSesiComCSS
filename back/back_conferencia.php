b56a9031513b38c66a9abbe2b517977a964c9844
Luny <143201033+LunyThefox@users.noreply.github.com>
2024-05-07 08:48:47 -0300

    conferência

--- back/back_conferencia.php
+++ back/back_conferencia.php
@@ -18,8 +18,25 @@ if($conexao -> connect_errno){
 }
 else{
 
-$SQL = 'INSERT INTO `conferencia` (`nome_m`,`n_empresa`,`modelo_v`,`eixos`,`PBT`,`CNPJ_empre`,`placa_veiculo`,`modelo_container`,`navio`,`destinatario`,`tipo_container`,`lacre`,`lacre_SIF`,`temperatura`,`IMO`,`n_ONU`, `containe*`) 
-    VALUES ("1","1","1","1","1","1","1","1","1","1","1","1","1","1","1","1", '".$containerDesgastado."');';
+$v1=$_POST['1'];
+$v2=$_POST['2'];
+$v3=$_POST['3'];
+$v4=$_POST['4'];
+$v5=$_POST['5'];
+$v6=$_POST['6'];
+$v7=$_POST['7'];
+$v8=$_POST['8'];
+$v9=$_POST['9'];
+$v10=$_POST['10'];
+$v11=$_POST['11'];
+$v12=$_POST['12'];
+$v13=$_POST['13'];
+$v14=$_POST['14'];
+$v15=$_POST['15'];
+$v16=$_POST['16'];
+
+$SQL = 'INSERT INTO `conferencia` (`nome_m`,`n_empresa`,`modelo_v`,`eixos`,`PBT`,`CNPJ_empre`,`placa_veiculo`,`modelo_container`,`navio`,`destinatario`,`tipo_container`,`lacre`,`lacre_SIF`,`temperatura`,`IMO`,`n_ONU`) 
+    VALUES ('"$v1"','"$v2"','"$v3"','"$v4"','"$v5"','"$v6"','"$v7"','"$v8"','"$v9"','"$v10"','"$v11"','"$v12"','"$v13"','"$v14"','"$v15"','"$v16"');';
 //Inserir no DB
 
 $resultado = $conexao -> query($SQL);
