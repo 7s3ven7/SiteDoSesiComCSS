785e0776056cc197167db04b351c7bfe5da072a3
Luny <143201033+LunyThefox@users.noreply.github.com>
2024-05-07 08:55:56 -0300

    save

--- back/back_conferencia.php
+++ back/back_conferencia.php
@@ -18,22 +18,22 @@ if($conexao -> connect_errno){
 }
 else{
 
-$v1=$_POST['1'];`nome_m`,`modelo_v`,`eixos`,`PBT`,`CNPJ_empre`,`placa_veiculo`,`modelo_container`,`navio`,`destinatario`,`tipo_container`,`lacre`,`lacre_SIF`,`temperatura`,`IMO`,`n_ONU`
-$v2=$_POST['2'];n_empresa
-$v3=$_POST['3'];
-$v4=$_POST['4'];
-$v5=$_POST['5'];
-$v6=$_POST['6'];
-$v7=$_POST['7'];
-$v8=$_POST['8'];
-$v9=$_POST['9'];
-$v10=$_POST['10'];
-$v11=$_POST['11'];
-$v12=$_POST['12'];
-$v13=$_POST['13'];
-$v14=$_POST['14'];
-$v15=$_POST['15'];
-$v16=$_POST['16'];
+$v1=$_POST['1']; //nome_m ``,`PBT`,`CNPJ_empre`,`placa_veiculo`,`modelo_container`,`navio`,`destinatario`,`tipo_container`,`lacre`,`lacre_SIF`,`temperatura`,`IMO`,`n_ONU`
+$v2=$_POST['2']; //n_empresa
+$v3=$_POST['3']; //modelo_v
+$v4=$_POST['4']; //eixos
+$v5=$_POST['5']; //
+$v6=$_POST['6']; //
+$v7=$_POST['7']; //
+$v8=$_POST['8']; //
+$v9=$_POST['9']; //
+$v10=$_POST['10']; //
+$v11=$_POST['11']; //
+$v12=$_POST['12']; //
+$v13=$_POST['13']; //
+$v14=$_POST['14']; //
+$v15=$_POST['15']; //
+$v16=$_POST['16']; //
 
 $SQL = 'INSERT INTO `conferencia` (`nome_m`,`n_empresa`,`modelo_v`,`eixos`,`PBT`,`CNPJ_empre`,`placa_veiculo`,`modelo_container`,`navio`,`destinatario`,`tipo_container`,`lacre`,`lacre_SIF`,`temperatura`,`IMO`,`n_ONU`) 
     VALUES ('".$v1."','".$v2."','".$v3."','".$v4."','".$v5."','".$v6."','".$v7."','".$v8."','".$v9."','".$v10."','".$v11."','".$v12."','".$v13."','".$v14."','".$v15."','".$v16."');';
