df13a169e6ca8c68096761e68d98ae5ac0437705
Luny <143201033+LunyThefox@users.noreply.github.com>
2024-05-07 08:33:31 -0300

    Back

--- back/back_conferencia.php
+++ back/back_conferencia.php
@@ -6,14 +6,20 @@ $DB = "dados";
 
 $conexao = new mysqli($hostname,$name,$password,$DB);//Tenta conexão com o DB
 
+/*if(isset($_POST['nome da caixa'])){
+    $containerDesgastado = 1;
+} else {
+    $containerDesgastado = 0;
+}
+*/
 if($conexao -> connect_errno){
     echo "Failed conection: " .$conexao -> connect_error; //erro caso não consiga conectar ao DB
     exit();
 }
 else{
 
-$SQL = 'INSERT INTO `conferencia` (`nome_m`,`n_empresa`,`modelo_v`,`eixos`,`PBT`,`CNPJ_empre`,`placa_veiculo`,`modelo_container`,`navio`,`destinatario`,`tipo_container`,`lacre`,`lacre_SIF`,`temperatura`,`IMO`,`n_ONU`) 
-    VALUES ("1","1","1","1","1","1","1","1","1","1","1","1","1","1","1","1");';
+$SQL = 'INSERT INTO `conferencia` (`nome_m`,`n_empresa`,`modelo_v`,`eixos`,`PBT`,`CNPJ_empre`,`placa_veiculo`,`modelo_container`,`navio`,`destinatario`,`tipo_container`,`lacre`,`lacre_SIF`,`temperatura`,`IMO`,`n_ONU`, `containe*`) 
+    VALUES ("1","1","1","1","1","1","1","1","1","1","1","1","1","1","1","1", '".$containerDesgastado."');';
 //Inserir no DB
 
 $resultado = $conexao -> query($SQL);
