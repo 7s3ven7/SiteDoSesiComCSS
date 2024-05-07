b20d24c193039f7a93232a46e9d40c7b720e5cdf
Luny <143201033+LunyThefox@users.noreply.github.com>
2024-05-07 09:46:54 -0300

    save

---
+++ back/back_qualitativo.php
@@ -0,0 +1,128 @@
+<?php
+if(isset($_POST['nome da caixa'])){
+    $containerDesgastado = 1;
+} else {
+    $containerDesgastado = 0;
+}
+
+if(isset($_POST['nome da caixa'])){
+    $containerDesgastado = 1;
+} else {
+    $containerDesgastado = 0;
+}
+
+if(isset($_POST['nome da caixa'])){
+    $containerDesgastado = 1;
+} else {
+    $containerDesgastado = 0;
+}
+
+if(isset($_POST['nome da caixa'])){
+    $containerDesgastado = 1;
+} else {
+    $containerDesgastado = 0;
+}
+
+if(isset($_POST['nome da caixa'])){
+    $containerDesgastado = 1;
+} else {
+    $containerDesgastado = 0;
+}
+
+if(isset($_POST['nome da caixa'])){
+    $containerDesgastado = 1;
+} else {
+    $containerDesgastado = 0;
+}
+
+if(isset($_POST['nome da caixa'])){
+    $containerDesgastado = 1;
+} else {
+    $containerDesgastado = 0;
+}
+
+if(isset($_POST['nome da caixa'])){
+    $containerDesgastado = 1;
+} else {
+    $containerDesgastado = 0;
+}
+
+if(isset($_POST['nome da caixa'])){
+    $containerDesgastado = 1;
+} else {
+    $containerDesgastado = 0;
+}
+
+if(isset($_POST['nome da caixa'])){
+    $containerDesgastado = 1;
+} else {
+    $containerDesgastado = 0;
+}
+
+if(isset($_POST['nome da caixa'])){
+    $containerDesgastado = 1;
+} else {
+    $containerDesgastado = 0;
+}
+
+if(isset($_POST['nome da caixa'])){
+    $containerDesgastado = 1;
+} else {
+    $containerDesgastado = 0;
+}
+
+if(isset($_POST['nome da caixa'])){
+    $containerDesgastado = 1;
+} else {
+    $containerDesgastado = 0;
+}
+
+if(isset($_POST['nome da caixa'])){
+    $containerDesgastado = 1;
+} else {
+    $containerDesgastado = 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+?>
