0a1df612d101a9958f37e385b0d412ba13ef11e5
Wesley_G_Rodrigues <125270476+7s3ven7@users.noreply.github.com>
2024-05-14 07:51:46 -0300

    save

--- Professor/action_logar.php
+++ Professor/action_logar.php
@@ -15,14 +15,6 @@ if ($conexao->connect_errno) {
 } else {
 	$nome = $conexao->real_escape_string($_POST["nome_conta"]);
 	$senha = $conexao->real_escape_string($_POST["senha_conta"]);
-<<<<<<< HEAD
-	$SQL = "SELECT `id`, `tipo`, `senha` FROM `usuario` WHERE `tipo` = '".$nome."' AND `senha` = '".$senha."'";
-	$resultado = $conexao->query($SQL);
-	if ($resultado->num_rows != 0) {
-		$row = $resultado->fetch_array();
-		$_SESSION['id'] = $row[0];
-		$_SESSION['tipo'] = $row[1];
-=======
 	$SQL = "SELECT `tipo`, `nome`, `senha` FROM `usuario` WHERE `nome` = '" . $nome . "' AND `senha` = '" . $senha . "'";
 
 	$result = $conexao->query($SQL);
@@ -30,7 +22,6 @@ if ($conexao->connect_errno) {
 		$row = $result->fetch_array();
 		$_SESSION['tipo'] = $row[0];
 		$_SESSION['nome'] = $row[1];
->>>>>>> 5703c25d37ed434aa9ffab42d54b5582f4994124
 		$_SESSION['senha'] = $row[2];
 		if ($row[0] == "Professor" and $row[2] == $senha) {
 			$conexao->close();
