26d59bc90a612a02c2ee3705e91729269bbf4406
Luny <143201033+LunyThefox@users.noreply.github.com>
2024-05-14 07:52:20 -0300

    ssadas

--- Professor/action_logar.php
+++ Professor/action_logar.php
@@ -15,35 +15,25 @@ if ($conexao->connect_errno) {
 } else {
 	$nome = $conexao->real_escape_string($_POST["nome_conta"]);
 	$senha = $conexao->real_escape_string($_POST["senha_conta"]);
-<<<<<<< HEAD
 	$SQL = "SELECT `id`, `tipo`, `senha` FROM `usuario` WHERE `tipo` = '".$nome."' AND `senha` = '".$senha."'";
 	$resultado = $conexao->query($SQL);
 	if ($resultado->num_rows != 0) {
 		$row = $resultado->fetch_array();
 		$_SESSION['id'] = $row[0];
 		$_SESSION['tipo'] = $row[1];
-=======
-	$SQL = "SELECT `tipo`, `nome`, `senha` FROM `usuario` WHERE `nome` = '" . $nome . "' AND `senha` = '" . $senha . "'";
-
-	$result = $conexao->query($SQL);
-	if ($result->num_rows > 0) {
-		$row = $result->fetch_array();
-		$_SESSION['tipo'] = $row[0];
-		$_SESSION['nome'] = $row[1];
->>>>>>> 5703c25d37ed434aa9ffab42d54b5582f4994124
 		$_SESSION['senha'] = $row[2];
-		if ($row[0] == "Professor" and $row[2] == $senha) {
+		if ($row[1] == 'professor' and $senha == $row[2]) {
 			$conexao->close();
 			header('Location: tela_professor.html', true, 301);
 			exit();
-		} elseif ($row[0] == 'Aluno' and $row[2] == $senha) {
+		}elseif ($_SESSION['tipo'] != 'professor' and $senha == $row[2]) {
 			$conexao->close();
 			header('Location: tela.php', true, 301);
 			exit();
-		} else {
-			$conexao->close();
-			header('Location: index.html', true, 301);
 		}
+	}else {
+		$conexao->close();
+		header('Location: index.html', true, 301);
 	}
 }
 ?>
