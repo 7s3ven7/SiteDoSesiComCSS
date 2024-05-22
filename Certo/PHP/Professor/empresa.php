6c4fb100163c16fdb4d8b783df01262bd9644a51
Wesley_G_Rodrigues <125270476+7s3ven7@users.noreply.github.com>
2024-05-22 07:59:19 -0300

    save

---
+++ Certo/PHP/Professor/empresa.php
@@ -0,0 +1,27 @@
+<?php
+$hostname = "127.0.0.1";
+$name = "root";
+$password = "root";
+$DB = "dados";
+
+$conexao = new mysqli($hostname, $name, $password, $DB);//Tenta conexão com o DB
+
+if ($conexao->connect_errno) {
+    echo "Failed conection: " . $conexao->connect_error; //erro caso não consiga conectar ao DB
+    exit();
+} else {
+    $v1 = $_POST['1']; //CNPJ
+    $v2 = $_POST['2']; //Nome do gerente
+    $v3 = $_POST['3']; //Email
+    $v4 = $_POST['4']; //Telefone
+    $v5 = $_POST['5']; //CEP
+
+    $SQL = 'INSERT INTO `nossa_empresa_p_1` (`CNPJ_empresa`,`gerente`,`email`,`fone`,`CEP`) 
+    VALUES (' . $v1 . ',' . $v2 . ',' . $v3 . ',' . $v4 . ',' . $v5 . ');';
+    //Inserir no DB
+    $resultado = $conexao->query($SQL);
+
+    $conexao->close();
+    header("Location: ../../HTML/movimentacao.html"); //Envia para a tela de Login ao Cadastrar
+}
+?>
