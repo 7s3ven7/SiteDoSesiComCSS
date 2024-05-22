476266249d2df573bf61cf41f1816b24579afdd9
Luny <143201033+LunyThefox@users.noreply.github.com>
2024-05-22 10:21:09 -0300

    save

--- Certo/PHP/Aluno/quantitativo.php
+++ Certo/PHP/Aluno/quantitativo.php
@@ -2,9 +2,9 @@
 
 <head>
     <link rel="stylesheet" href="../../CSS/site.css">
-    <meta http-equiv="Content-Type" content="text/html";charset="UTF-8">
+    <meta http-equiv="Content-Type" content="text/html" ;charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <meta name="viewport" content="width=device-width", initial-scale="1.0">
+    <meta name="viewport" content="width=device-width" , initial-scale="1.0">
 </head>
 
 <body><?php
