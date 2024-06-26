c99a85fa3d715de070842391785e245f3e804345
Matheus Jastrombek Fernandes <130985777+matheus-j-fernandes@users.noreply.github.com>
2024-06-26 12:16:52 -0300

    save

--- Certo/t_empresa_p.php
+++ Certo/t_empresa_p.php
@@ -131,6 +131,34 @@
                 echo'<div class="details-caixa-2"></div>';}}
                 echo'</form>';
             ?>
+            <?php
+$hostname = "127.0.0.1";
+$name = "root";
+$password = "root";
+$DB = "dados";
+
+$conexao = new mysqli($hostname, $name, $password, $DB);//Tenta conexão com o DB
+
+if ($conexao->connect_errno) {
+    echo "Failed conection: " . $conexao->connect_error; //erro caso não consiga conectar ao DB
+    exit();
+} else {
+    $v1 = $_POST['1']; //CNPJ
+    $v2 = $_POST['2']; //Nome do gerente
+    $v3 = $_POST['3']; //Email
+    $v4 = $_POST['4']; //Telefone
+    $v5 = $_POST['5']; //CEP
+
+    $SQL = 'INSERT INTO `nossa_empresa_p` (`CNPJ_e`,`gmail_e`,`fone_e`,`CEP_e`,`nome_e`) 
+    VALUES (' . $v1 . ',' . $v2 . ',' . $v3 . ',' . $v4 . ',' . $v5 . ');';
+    //Inserir no DB
+    $resultado = $conexao->query($SQL);
+
+    $conexao->close();
+    header("Location: t_empresa_p.php"); //Envia para a tela de Login ao Cadastrar
+}
+?>
+
     </div>
 
     <div id="tipo">Conta: Professor</div>
--- Certo/t_quantitativo_a.php
+++ Certo/t_quantitativo_a.php
@@ -166,6 +166,34 @@
             }
         }}
         ?>
+        <?php
+$hostname = "127.0.0.1";
+$name = "root";
+$password = "root";
+$DB = "dados";
+
+$conexao = new mysqli($hostname, $name, $password, $DB);//Tenta conexão com o DB
+if ($conexao->connect_errno) {
+    echo "Failed connection: " . $conexao->connect_error; //erro caso não consiga conectar ao DB
+    header("Location: t_a.php");
+    exit();
+} else {
+    $n_pedido=$_POST['n_pedido'];
+    $SQL = 'SELECT * FROM `produto_p` 
+    WHERE `n_pedido` = '.$n_pedido.';';
+    //Inserir no DB
+    $resultado = $conexao->query($SQL); //Envia para a tela de Login ao Cadastrar
+    if($resultado->num_rows != 0) //Caso a pesquisa no DB tenha resultado, ele puxa os dados "id" e "tipo" do DB
+	{
+		for($i=1;$i<=$resultado->num_rows;$i++){
+			$row = $resultado -> fetch_array();
+		}
+	} else {
+		$conexao -> close();
+		exit();
+	}
+}
+?>
     </div>
     </div>
 </body>
