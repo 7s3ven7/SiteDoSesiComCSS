9029fd8c11b6ae29dea731cc57bef0cde328382e
Wesley_G_Rodrigues <125270476+7s3ven7@users.noreply.github.com>
2024-06-26 08:57:47 -0300

    save

--- Certo/c_cliente_p.php
+++ Certo/c_cliente_p.php
@@ -10,6 +10,27 @@ if ($conexao->connect_errno) {
     echo "Failed conection: " . $conexao->connect_error; //erro caso não consiga conectar ao DB
     exit();
 } else {
+
+    if(isset($_POST['vezes'])){
+        $i = $_POST['vezes'];
+        if($i <= 0){
+        }else{
+        $l = $i;
+        $numero = 1;
+        $v1 = 'cnpj';
+        $v2 = 'nome_cliente';
+        $v3 = 'gmail';
+        $v4 = 'cep';
+        $v5 = 'telefone';
+        for($i;$i>0;$i){
+        $i -= 1;
+$numero += 1;
+$v1 = $v1.'p';
+$v2 = $v2.'p';
+$v3 = $v3.'p';
+$v4 = $v4.'p';
+$v5 = $v5.'p';
+        }
     $v1 = $_POST['1']; //CPF
     $v2 = $_POST['2']; //Nome do Cliente
     $v3 = $_POST['3']; //Gmail
@@ -18,7 +39,7 @@ if ($conexao->connect_errno) {
 
     $SQL = 'INSERT INTO `clientes_p` (`CNPJ_c`,`nome_c`,`gmail_c`,`CEP_c`,`fone_c`) 
     VALUES (' . $v1 . ',' . $v2 . ',' . $v3 . ',' . $v4 . ',' . $v5 . ');';
-    //Inserir no DB
+        }}
     $resultado = $conexao->query($SQL);
 
     $conexao->close();
