05fc507d48e73ee9b5af2772d129c71031e2247a
LunyThefox <143201033+LunyThefox@users.noreply.github.com>
2024-07-02 08:11:03 -0300

    save

--- Certo/t_empresa_p.php
+++ Certo/t_empresa_p.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 
 <head>
-    <link rel="stylesheet" href="a_site.css">
+    <link rel="stylesheet" href="">
     <meta http-equiv="Content-Type" content="text/html;charset=UTF-8">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -13,76 +13,76 @@
 ?>
 
 <body>
-    <div class="fundo"></div>
-    <details class="details-all">
-        <summary class="details-big">Cadastros</summary>
+    <div class=""></div>
+    <details class="">
+        <summary class="">Cadastros</summary>
         <form method="POST"
             action="t_fornecedor_p.php?nome=<?php echo $nome;?>&nome_atividade=<?php echo $nome_atividade;?>">
-            <input class="details-small" type="submit" value="Fornecedor"></div>
+            <input class="" type="submit" value="Fornecedor"></div>
         </form>
         <form method="POST"
             action="t_empresa_p.php?nome=<?php echo $nome;?>&nome_atividade=<?php echo $nome_atividade;?>">
-            <input class="details-small" type="submit" value="Empresa"></div>
+            <input class="" type="submit" value="Empresa"></div>
         </form>
         <details>
-            <summary class="details-small-more">Cliente</summary>
+            <summary class="">Cliente</summary>
             <form method="POST"
                 action="t_cliente_p.php?nome=<?php echo $nome;?>&nome_atividade=<?php echo $nome_atividade;?>">
-                <input class="details-small" type="submit" value="Cadastro"></div>
+                <input class="" type="submit" value="Cadastro"></div>
             </form>
             <form method="POST" action="t_p.php?nome=<?php echo $nome;?>&nome_atividade=<?php echo $nome_atividade;?>">
-                <input class="details-small" type="submit" value="Pedido"></div>
+                <input class="" type="submit" value="Pedido"></div>
             </form>
         </details>
     </details>
-    <details class="details-all">
-        <summary class="details-big">Produto</summary>
+    <details class="">
+        <summary class="">Produto</summary>
         <form method="POST"
             action="t_produto_p.php?nome=<?php echo $nome;?>&nome_atividade=<?php echo $nome_atividade;?>">
-            <input class="details-small" type="submit" value="Cadastro">
+            <input class="" type="submit" value="Cadastro">
         </form>
     </details>
-    <details class="details-all">
-        <summary class="details-big">Recebimento</summary>
+    <details class="">
+        <summary class="">Recebimento</summary>
         <form method="POST"
             action="t_quantitativo_r_p.php?nome=<?php echo $nome;?>&nome_atividade=<?php echo $nome_atividade;?>">
-            <input class="details-small" type="submit" value="Quantitativo"></div>
+            <input class="" type="submit" value="Quantitativo"></div>
         </form>
         <form method="POST"
             action="t_nota_r_p.php?nome=<?php echo $nome;?>&nome_atividade=<?php echo $nome_atividade;?>">
-            <input class="details-small" type="submit" value="Nota Fiscal">
+            <input class="" type="submit" value="Nota Fiscal">
         </form>
     </details>
-    <details class="details-all">
-        <summary class="details-big">Expedição</summary>
+    <details class="">
+        <summary class="">Expedição</summary>
         <form method="POST"
             action="t_quantitativo_e_p.php?nome=<?php echo $nome;?>&nome_atividade=<?php echo $nome_atividade;?>">
-            <input class="details-small" type="submit" value="Quantitativo">
+            <input class="" type="submit" value="Quantitativo">
         </form>
         <form method="POST"
             action="t_nota_e_pr.php?nome=<?php echo $nome;?>&nome_atividade=<?php echo $nome_atividade;?>">
-            <input class="details-small" type="submit" value="Nota Fiscal">
+            <input class="" type="submit" value="Nota Fiscal">
         </form>
     </details>
-    <details class="details-all">
-        <summary class="details-big">Expedição</summary>
+    <details class="">
+        <summary class="">Expedição</summary>
         <form action="t_quantitativo_e_p.php?nome=<?php echo $nome;?>&nome_atividade=<?php echo $nome_atividade;?>">
-            <input class="details-small" type="submit" value="Quantitativo">
+            <input class="" type="submit" value="Quantitativo">
         </form>
         <form action="t_nota_e_pr.php?nome=<?php echo $nome;?>&nome_atividade=<?php echo $nome_atividade;?>">
-            <input class="details-small" type="submit" value="Nota Fiscal">
+            <input class="" type="submit" value="Nota Fiscal">
         </form>
     </details>
-    <div class="details-caixa"></div>
-    <div class="menu">Menu</div>
-    <div class="caixa">
+    <div class=""></div>
+    <div class="">Menu</div>
+    <div class="">
     </div>
 
-    <div class="caixa-fixa">
+    <div class="">
         <form method="POST" action="t_empresa_p.php">
-            <div class="texto-produto">Quantas empresas irás registrar: <input class="botao-produto" type="number"
-                    name="vezes" placeholder="Número de vezes*"></div>
-            <input class="enviar-numero-produto" type="submit">
+            <div class="">Quantas empresas irás registrar: <input class="" type="number" name="vezes"
+                    placeholder="Número de vezes*"></div>
+            <input class="" type="submit">
         </form>
         <?php
             if(isset($_POST['vezes'])){
@@ -90,7 +90,7 @@
                     if($i <= 0){
                         echo '
                         <br><br>
-                        <div class="texto-produto">Digite um número superior!</div>';
+                        <div class="">Digite um número superior!</div>';
                     }else{
                     $l = $i;
                     $numero = 1;
@@ -104,23 +104,23 @@
                     for($i;$i>0;$i){
                     $i -= 1;
                     echo'
-            <div class="numero-produto">'.$numero.'° Empresa</div>
+            <div class="">'.$numero.'° Empresa</div>
             <br>
-            <table class="tabela-mini">
+            <table class="">
                 <tr>
-                    <td class="texto-tabela-mini">CNPJ: </td>
-                    <td><input class="botao-tabela" type="text" name="'.$v1.'" placeholder="00.000.000/0000-00"></td>
-                    <td class="texto-tabela-mini">Nome da empresa: </td>
-                    <td><input class="botao-tabela" type="text" name="'.$v2.'" placeholder="Nome"></td>
+                    <td class="">CNPJ: </td>
+                    <td><input class="" type="text" name="'.$v1.'" placeholder="00.000.000/0000-00"></td>
+                    <td class="">Nome da empresa: </td>
+                    <td><input class="" type="text" name="'.$v2.'" placeholder="Nome"></td>
                 <tr>           
-                    <td class="texto-tabela-mini">Gmail: </td>
-                    <td><input class="botao-tabela" type="text" name="'.$v3.'" placeholder="exemplo@gmail.com"></td>
+                    <td class="">Gmail: </td>
+                    <td><input class="" type="text" name="'.$v3.'" placeholder="exemplo@gmail.com"></td>
                 </tr>
                 <tr>
-                    <td class="texto-tabela-mini">CEP: </td>
-                    <td><input class="botao-tabela" type="text" name="'.$v4.'" placeholder="0000-000"></td>
-                    <td class="texto-tabela-mini">Telefone: </td>
-                    <td><input class="botao-tabela" type="text" name="'.$v5.'" placeholder="(00) 00000-0000 "></td>
+                    <td class="">CEP: </td>
+                    <td><input class="" type="text" name="'.$v4.'" placeholder="0000-000"></td>
+                    <td class="">Telefone: </td>
+                    <td><input class="" type="text" name="'.$v5.'" placeholder="(00) 00000-0000 "></td>
                 </tr>
             </table>';
             $numero += 1;
@@ -157,9 +157,9 @@ if ($conexao->connect_errno) {
     $conexao->close();
     header("Location: t_empresa_p.php"); //Envia para a tela de Login ao Cadastrar
 }
-                echo '<table class="table">';}
-                echo '<input class="botao" type="submit">';}}
-                echo'<div class="details-caixa-2"></div>';
+                echo '<table class="">';}
+                echo '<input class="" type="submit">';}}
+                echo'<div class=""></div>';
                 echo'</form>';
             ?>
 
