eb20001ba41f1ef40fd7ecd298ebd683ab486bfb
LunyThefox <143201033+LunyThefox@users.noreply.github.com>
2024-07-30 07:59:49 -0300

    sa

--- Certo/t_turma.php
+++ Certo/t_turma.php
@@ -293,3 +293,12 @@
 </body>
 
 </html>
+
+<!--
+var button = document.querySelector("button");
+
+button.onclick = function () {
+  var nome = prompt("Qual é o seu nome?");
+  alert("Olá " + nome + ", é um prazer te ver!");
+};
+-->
