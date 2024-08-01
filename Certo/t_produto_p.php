308d9f45f8d9fe3d5f6abf6a3499dadbfa173e43
Matheus Jastrombek Fernandes <130985777+matheus-j-fernandes@users.noreply.github.com>
2024-08-01 09:39:51 -0300

    save

--- Certo/t_produto_p.php
+++ Certo/t_produto_p.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 
 <head>
-    <link rel="stylesheet" href="">
+    <link rel="stylesheet" href="style.css">
     <meta http-equiv="Content-Type" content="text/html;charset=UTF-8">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -21,7 +21,7 @@
         ?>
 
 <body>
-<div class='caixa-menu-geral'>
+    <div class='caixa-menu-geral'>
         <div class='espaco'></div>
         <details>
             <summary>Criações</summary>
@@ -76,168 +76,168 @@
         </details>
     </div>
     <div class="menu">Menu</div>
-    <div class="">
-    </div>
-
-    <div class="">
+    <div class='caixa-tela-informacao-geral'>
         <form method="POST" action="t_produto_p.php?nome=<?php redirect()?>">
             <div class="">Quantos produtos você irá registrar: <input class="botao-produto" type="number" name="vezes"
                     placeholder="Número de vezes*"></div>
             <input class="" type="submit">
         </form>
-<?php
-    if(isset($_POST['vezes'])){
-        $i = $_POST['vezes'];
-        if($i <= 0){
-            echo '
-            <br><br>
-            <div class="">Digite um número superior!</div>';
-        }else{
-            $l = $i;
-            $numero = 1;
-            $v1 = 'cod_fone';
-            $v2 = 'tipo';
-            $v3 = 'validade';
-            $v4 = 'lote';
-            $v5 = 'nome';
-            $v6 = 'marca';
-            $v7 = 'numero';
-            $v8 = 'unidade';
-            $v9 = 'quantidade_unidade';
-            $v10 = 'quantidade_produto';
-            $v11 = 'valor_unidade';
-            $v12 = 'peso';
-            echo'<form method="POST" action="c_produto_p.php?l='.$l.'&nome=';redirect();echo'">';
-            echo'<br>';
-            for($i;$i>0;$i){
-                $i -= 1;
-                echo'
-                <div class="">'.$numero.'° Produto</div>
-                <br>
-                <table class="">
-                    <tr>
-                        <td class="">Código do fornecedor: </td>
-                        <td><input class="" type="text" name="'.$v1.'" placeholder="1"></td>
-                        <td class="">Tipo: </td>
-                        <td><input class="" type="text" name="'.$v2.'" placeholder="caixa"></td>
-                        <td class="">Validade: </td>
-                        <td><input class="" type="date" name="'.$v3.'"></td>
-                    </tr>
-                    <tr>
-                        <td class="">Lote: </td>
-                        <td><input class="" type="text" name="'.$v4.'" placeholder="1"></td>
-                        <td class="">Nome: </td>
-                        <td><input class="" type="text" name="'.$v5.'" placeholder="empresa"></td>
-                        <td class="">Marca do produto: </td>
-                        <td><input class="" type="text" name="'.$v6.'" placeholder="marca"></td>
-                    </tr>
-                    <tr>
-                        <td class="">Número do pedido: </td>
-                        <td><input class="" type="text" name="'.$v7.'" placeholder="1"></td>
-                        <td class="">Unidade: </td>
-                        <td><input class="" type="text" name="'.$v8.'" placeholder="1"></td>
-                        <td class="">Quantidade de unidade: </td>
-                        <td><input class="" type="text" name="'.$v9.'" placeholder="1"></td>
-                    </tr>
-                    <tr>
-                        <td class="">Quantidade de produto: </td>
-                        <td><input class="" type="text" name="'.$v10.'" placeholder="1"></td>
-                        <td class="">Valor da unidade: </td>
-                        <td><input class="" type="text" name="'.$v11.'" placeholder="1"></td>
-                        <td class="">Peso em kilo grama: </td>
-                        <td><input class="" type="text" name="'.$v12.'" placeholder="1"></td>
-                    </tr>
-                </table>';
-                $numero += 1;
-                $v1 = $v1.'p';
-                $v2 = $v2.'p';
-                $v3 = $v3.'p';
-                $v4 = $v4.'p';
-                $v5 = $v5.'p';
-                $v6 = $v6.'p';
-                $v7 = $v7.'p';
-                $v8 = $v8.'p';
-                $v9 = $v9.'p';
-                $v10 = $v10.'p';
-                $v11 = $v11.'p';
-                $v12 = $v12.'p';
-            }              
-            echo '<table class="">';
-            echo '<input class="" type="submit">';
-            echo'<div class=""></div>';
-        }
-    }
-    echo'</form>';
-?>
-        <html>
-<?php
-    $hostname = "127.0.0.1";
-    $name = "root";
-    $password = "root";
-    $DB = "dados";
-
-    $conexao = new mysqli($hostname, $name, $password, $DB);//Tenta conexão com o DB
-
-    if ($conexao->connect_errno) {
-        echo "Failed conection: " . $conexao->connect_error; //erro caso não consiga conectar ao DB
-        exit();
-    } else {
-        $z = $_GET['$l'];
-        $a = 'cod_fone';
-        $b = 'tipo';
-        $c = 'validade';
-        $d = 'lote';
-        $e = 'nome';
-        $f = 'marca';
-        $g = 'numero';
-        $h = 'unidade';
-        $i = 'quantidade_unidade';
-        $j = 'quantidade_produto';
-        $k = 'valor_unidade';
-        $l = 'peso';
-        while($z>0){
-            $v1 = $_POST[$a];
-            $v2 = $_POST[$b];
-            $v3 = $_POST[$c];
-            $v4 = $_POST[$d];
-            $v5 = $_POST[$e];
-            $v6 = $_POST[$f];
-            $v7 = $_POST[$g];
-            $v8 = $_POST[$h];
-            $v9 = $_POST[$i];
-            $v10 = $_POST[$j];
-            $v11 = $_POST[$k];
-            $v12 = $_POST[$l];
-            $vtotal = $v11 * ($v9*$v10); //cod inter id atividade
-            for($zz = 1;$zz>0;$zz){
-                $SQL = 'INSERT INTO `produto_p` (`cod_forne`,`tipo`,`date_v`,`lote`,`nome_p`,`marca_p`,`id_atividade'./*.esse_aqui_tem_problema.*/'`,`und`,`quant_und`,`quant_prod`,`valor_und`,`Kg`,`valor_total'./*.esse_aqui_tem_problema.*/'`) 
-                VALUES ("' . $v1 . '","' . $v2 . '","' . $v3 . '","' . $v4 . '","' . $v5 . '","'.$v6.'","' . $v7 . '","' . $v8 . '","' . $v9 . '","' . $v10 . '","' . $v11 . '","'.$v12.'","'.$vtotal.'")';
-                $resultado = $conexao->query($SQL);
-                $zz -= 1;
+        <?php
+            $hostname = "127.0.0.1";
+            $name = "root";
+            $password = "root";
+            $DB = "dados";
+        
+            $conexao = new mysqli($hostname, $name, $password, $DB);//Tenta conexão com o DB
+        
+            if ($conexao->connect_errno) {
+                echo "Failed conection: " . $conexao->connect_error; //erro caso não consiga conectar ao DB
+                exit();
+            } else {
+                if(isset($_POST['vezes'])){ exibir();}
+                if(isset($_POST['$l'])){ salvar();}
             }
-        $zz = 1;
-        $a = $a.'p';
-        $b = $b.'p';
-        $c = $c.'p';
-        $d = $d.'p';
-        $e = $e.'p';
-        $f = $f.'p';
-        $g = $g.'p';
-        $h = $h.'p';
-        $i = $i.'p';
-        $j = $j.'p';
-        $k = $k.'p';
-        $l = $l.'p';
-        $z -= 1;
-        }
-        $conexao->close();
-        header("Location: t_produto_p.php");
-    }
-?>
-
-        </html>
+            ?>
     </div>
-    <div id="">Professor - <?php echo $nome;?></div>
+    <div class='conta-geral'>Professor - <?php echo $nome;?></div>
 </body>
-
+<?php
+function exibir(){
+if(isset($_POST['vezes'])){
+    $i = $_POST['vezes'];
+    if($i <= 0){
+        echo '
+        <br><br>
+        <div class="">Digite um número superior!</div>';
+    }else{
+        $l = $i;
+        $numero = 1;
+        $v1 = 'cod_fone';
+        $v2 = 'tipo';
+        $v3 = 'validade';
+        $v4 = 'lote';
+        $v5 = 'nome';
+        $v6 = 'marca';
+        $v7 = 'numero';
+        $v8 = 'unidade';
+        $v9 = 'quantidade_unidade';
+        $v10 = 'quantidade_produto';
+        $v11 = 'valor_unidade';
+        $v12 = 'peso';
+        echo'<form method="POST" action="t_produto_p.php?$l='.$l.'&nome=';redirect();echo'">';
+        echo'<br>';
+        for($i;$i>0;$i){
+            $i -= 1;
+            echo'
+            <div class="">'.$numero.'° Produto</div>
+            <br>
+            <table class="">
+                <tr>
+                    <td class="">Código do fornecedor: </td>
+                    <td><input class="texto-pequeno-produto" type="text" name="'.$v1.'" placeholder="1"></td>
+                    <td class="">Tipo: </td>
+                    <td><input class="" type="text" name="'.$v2.'" placeholder="caixa"></td>
+                    <td class="">Validade: </td>
+                    <td><input class="" type="date" name="'.$v3.'"></td>
+                </tr>
+                <tr>
+                    <td class="">Lote: </td>
+                    <td><input class="" type="text" name="'.$v4.'" placeholder="1"></td>
+                    <td class="">Nome: </td>
+                    <td><input class="" type="text" name="'.$v5.'" placeholder="empresa"></td>
+                    <td class="">Marca do produto: </td>
+                    <td><input class="" type="text" name="'.$v6.'" placeholder="marca"></td>
+                </tr>
+                <tr>
+                    <td class="">Número do pedido: </td>
+                    <td><input class="" type="text" name="'.$v7.'" placeholder="1"></td>
+                    <td class="">Unidade: </td>
+                    <td><input class="" type="text" name="'.$v8.'" placeholder="1"></td>
+                    <td class="">Quantidade de unidade: </td>
+                    <td><input class="" type="text" name="'.$v9.'" placeholder="1"></td>
+                </tr>
+                <tr>
+                    <td class="">Quantidade de produto: </td>
+                    <td><input class="" type="text" name="'.$v10.'" placeholder="1"></td>
+                    <td class="">Valor da unidade: </td>
+                    <td><input class="" type="text" name="'.$v11.'" placeholder="1"></td>
+                    <td class="">Peso em kilo grama: </td>
+                    <td><input class="" type="text" name="'.$v12.'" placeholder="1"></td>
+                </tr>
+            </table>';
+            $numero += 1;
+            $v1 = $v1.'p';
+            $v2 = $v2.'p';
+            $v3 = $v3.'p';
+            $v4 = $v4.'p';
+            $v5 = $v5.'p';
+            $v6 = $v6.'p';
+            $v7 = $v7.'p';
+            $v8 = $v8.'p';
+            $v9 = $v9.'p';
+            $v10 = $v10.'p';
+            $v11 = $v11.'p';
+            $v12 = $v12.'p';
+        }              
+        echo '<table class="">';
+        echo '<input class="" type="submit">';
+        echo'<div class=""></div>';
+        echo'</form>';
+    }
+}
+}
+function salvar(){
+if(isset($_GET['$l'])){
+    $z = $_GET['$l'];
+    $a = 'cod_fone';
+    $b = 'tipo';
+    $c = 'validade';
+    $d = 'lote';
+    $e = 'nome';
+    $f = 'marca';
+    $g = 'numero';
+    $h = 'unidade';
+    $i = 'quantidade_unidade';
+    $j = 'quantidade_produto';
+    $k = 'valor_unidade';
+    $l = 'peso';
+    while($z>0){
+        $v1 = $_POST[$a];
+        $v2 = $_POST[$b];
+        $v3 = $_POST[$c];
+        $v4 = $_POST[$d];
+        $v5 = $_POST[$e];
+        $v6 = $_POST[$f];
+        $v7 = $_POST[$g];
+        $v8 = $_POST[$h];
+        $v9 = $_POST[$i];
+        $v10 = $_POST[$j];
+        $v11 = $_POST[$k];
+        $v12 = $_POST[$l];
+        $vtotal = $v11 * ($v9*$v10); //cod inter id atividade
+        for($zz = 1;$zz>0;$zz){
+            $SQL = 'INSERT INTO `produto_p` (`cod_forne`,`tipo`,`date_v`,`lote`,`nome_p`,`marca_p`,`id_atividade'./*.esse_aqui_tem_problema.*/'`,`und`,`quant_und`,`quant_prod`,`valor_und`,`Kg`,`valor_total'./*.esse_aqui_tem_problema.*/'`) 
+            VALUES ("' . $v1 . '","' . $v2 . '","' . $v3 . '","' . $v4 . '","' . $v5 . '","'.$v6.'","' . $v7 . '","' . $v8 . '","' . $v9 . '","' . $v10 . '","' . $v11 . '","'.$v12.'","'.$vtotal.'")';
+            $resultado = $conexao->query($SQL);
+            $zz -= 1;
+        }
+    $zz = 1;
+    $a = $a.'p';
+    $b = $b.'p';
+    $c = $c.'p';
+    $d = $d.'p';
+    $e = $e.'p';
+    $f = $f.'p';
+    $g = $g.'p';
+    $h = $h.'p';
+    $i = $i.'p';
+    $j = $j.'p';
+    $k = $k.'p';
+    $l = $l.'p';
+    $z -= 1;
+    $conexao->close();
+    header("Location: t_produto_p.php");
+}
+}}
+?>
 </html>
