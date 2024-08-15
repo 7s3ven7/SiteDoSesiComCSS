51e4e6009378c9fa6bfeebf18e06a8f13a56ac15
Wesley_G_Rodrigues <wesley_g_rodrigues@estudante.sesisenai.org.br>
2024-08-15 09:15:14 -0300

    save

--- Certo/t_nota_r_a.php
+++ Certo/t_nota_r_a.php
@@ -17,6 +17,88 @@
             echo $nome;
         }
     }
+    function exibir(){
+        global $conexao;
+        $slq = 'SELECT * FROM `nota_r_p` WHERE `n_nota_r` = "'.$_POST['nota'].'"';
+        $resultado = $conexao->query($slq);
+        if($resultado->num_rows > 0){
+            $row = mysqli_fetch_array($resultado);
+            $CNPJ_e = $row['1'];
+            $eixos = $row['2'];
+            $id_atividade = $row['3'];
+            $CEP_f = $row['4'];
+            $lote = $row['5'];
+            $nome_e = $row['6'];
+            $CSP = $row['7'];
+            $fone_f = $row['8'];
+            $nome_motorista = $row['9'];
+            $CPF_motorista = $row['10'];
+            $CFOP = $row['11'];
+            $CNPJ_f = $row['12'];
+            $placa_vei = $row['13'];
+            $PBT = $row['14'];
+            $dunf = $row['15'];
+            $modelo_vei = $row['16'];
+            $CEP_e = $row['17'];
+            $NCM = $row['18'];
+            $data_emi = $row['19'];
+            $und = $row['20'];
+            $quant_und = $row['21'];
+            $quant_prod = $row['22'];
+            $cod_f = $row['23'];
+            $valor_und = $row['24'];
+            echo'<table class="tabela-t-nota-r-a">
+                    <tr>
+                    <td class="td-t-nota-r-a">'.$CNPJ_e.'</td>
+                    <td class="td-t-nota-r-a">'.$eixos.'</td>
+                    <td class="td-t-nota-r-a">'.$id_atividade.'</td>
+                    <td class="td-t-nota-r-a">'.$CEP_f.'</td>
+                    </tr>
+                    <tr>
+                    <td class="td-t-nota-r-a">'.$lote.'</td>
+                    <td class="td-t-nota-r-a">'.$nome_e.'</td>
+                    <td class="td-t-nota-r-a">'.$CSP.'</td>
+                    <td class="td-t-nota-r-a">'.$fone_f.'</td>
+                    </tr>
+                    <tr>
+                    <td class="td-t-nota-r-a">'.$nome_motorista.'</td>
+                    <td class="td-t-nota-r-a">'.$CPF_motorista.'</td>
+                    <td class="td-t-nota-r-a">'.$CFOP.'</td>
+                    <td class="td-t-nota-r-a">'.$CNPJ_f.'</td>
+                    </tr>
+                    <tr>
+                    <td class="td-t-nota-r-a">'.$placa_vei.'</td>
+                    <td class="td-t-nota-r-a">'.$PBT.'</td>
+                    <td class="td-t-nota-r-a">'.$dunf.'</td>
+                    <td class="td-t-nota-r-a">'.$modelo_vei.'</td>
+                    </tr>
+                    <tr>
+                    <td class="td-t-nota-r-a">'.$CEP_e.'</td>
+                    <td class="td-t-nota-r-a">'.$NCM.'</td>
+                    <td class="td-t-nota-r-a">'.$data_emi.'</td>
+                    <td class="td-t-nota-r-a">'.$und.'</td>
+                    </tr>
+                    <tr>
+                    <td class="td-t-nota-r-a">'.$quant_und.'</td>
+                    <td class="td-t-nota-r-a">'.$quant_prod.'</td>
+                    <td class="td-t-nota-r-a">'.$cod_f.'</td>
+                    <td class="td-t-nota-r-a">'.$valor_und.'</td>
+                    </tr>
+            </table>';
+        }else{
+            echo "<div class='error'>A nota fiscal a qual você procura não existe</div>";
+        }
+    }
+    $hostname = "127.0.0.1";
+    $name = "root";
+    $password = "root";
+    $DB = "dados";
+
+    $conexao = new mysqli($hostname, $name, $password, $DB);//Tenta conexão com o DB
+    if ($conexao->connect_errno) {
+        echo "Failed conection: " . $conexao->connect_error; //erro caso não consiga conectar ao DB
+        exit();
+    }
         ?>
 
 <body>
@@ -74,5 +156,10 @@
     </details>
 
     <div class="caixa-tela-informacao-geral">
+        <form method="POST" action="t_nota_r_a.php?nome=<?php redirect()?>">
+            <input type="number" name="nota">
+            <input type="submit" name="submit-nota">
+        </form>
+        <?php if(isset($_POST["nota"])){exibir();} ?>
     </div>
     </div>
