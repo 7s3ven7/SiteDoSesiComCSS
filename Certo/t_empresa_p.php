d8ad2c5668451dc4cfe1c223f483294645ee35e0
Matheus Jastrombek Fernandes <130985777+matheus-j-fernandes@users.noreply.github.com>
2024-08-20 11:13:40 -0300

    mk

--- Certo/t_empresa_p.php
+++ Certo/t_empresa_p.php
@@ -16,12 +16,38 @@
             echo $nome.'&nome_atividade='.$nome_atividade;
         }else{
             echo $nome;
-        }
+        }}
+    function cadastro(){
+        global $conexao;
+        $cnpj = $_POST['cnpj'];
+        $nome_e = $_POST['nome_e'];
+        $gmail = $_POST['gmail'];
+        $cep = $_POST['cep'];
+        $telefone = $_POST['telefone'];
+        $nome_atividade = $_GET['nome_atividade'];
+        $sql = 'INSERT INTO `empresa_p`(`CNPJ_e`,`gmail_e`,`fone_e`,`CEP_e`,`nome_e`,`id_atividade`) 
+        VALUES ("' . $cnpj . '","' . $gmail . '","' . $telefone . '","' . $cep . '","' . $nome_e . '","'.$nome_atividade.'");';
+        $resultado = $conexao -> query($sql);
+        header('location:t_empresa_p.php?nome='.$_GET['nome'].'&nome_atividade='.$_GET['nome_atividade']);
     }
+    $hostname = "127.0.0.1";
+    $name = "u935055604_wesley";
+    $password = "XwZX1383";
+    $DB = "u935055604_dados";
+    $conexao = new mysqli($hostname, $name, $password, $DB);//Tenta conexão com o DB
+        if ($conexao->connect_errno) {
+            echo "Failed conection: " . $conexao->connect_error; //erro caso não consiga conectar ao DB
+            exit();
+        }
+        else{
+            if(isset($_GET["cadastro"])){
+                cadastro();
+            }
+        }
         ?>
 
 <body>
-<div id='caixa-menu-geral'>
+    <div class='caixa-menu-geral'>
         <div class='espaco'></div>
         <details>
             <summary>Criações</summary>
@@ -75,118 +101,28 @@
             </form>
         </details>
     </div>
-    <div ="menu">Menu</div>
+    <div class="menu">Menu</div>
     <div class="caixa-tela-informacao-geral">
-        <div class="caixa-esquerda-empresa">
-        <div class="texto-grande-empresa">Criação de empresas</div>
-            <div class="caixa-esquerda-empresa-dentro">
-                <form method="POST" action='t_empresa_p.php?nome=<?php redirect()?>'>
-                <div class="texto-medio-empresa">Quantas empresas irá registrar? </div>
-                    <input class="botao-empresa-input" type="number" placeholder="qtd. empresas" name="qtd_empresas">
-                <br>
-                    <input class="botao-empresa-submit" type="submit" value="Confirmar">
-                <?php
-    if(isset($_POST['qtd_empresas'])){
-        $i = $_POST['qtd_empresas'];
-        if($i <= 0){
-            echo '
-            <br><br>
-            <div class="">Digite um número superior!</div>';
-        }else{
-            $l = $i;
-            $numero = 1;
-            $v1 = 'cnpj';
-            $v2 = 'nome_empresa';
-            $v3 = 'gmail';
-            $v4 = 'cep';
-            $v5 = 'telefone';
-            echo'<form method="POST" action="t_empresa_p.php?$l='.$l.'">';
-            echo'<br>';
-            for($i;$i>0;$i){
-                $i -= 1;
-                echo'
-                <div class="texto-cinza-empresa">'.$numero.'° Empresa</div>
-                <br>
-                <table class="tabela-empresa">
-                    <tr>
-                        <td class="texto-pequeno-empresa">CNPJ: </td>
-                        <td><input class="" type="text" name="'.$v1.'" placeholder="00.000.000/0000-00"></td>
-                        <td class="texto-pequeno-empresa">Nome da empresa: </td>
-                        <td><input class="" type="text" name="'.$v2.'" placeholder="Nome"></td>
-                    <tr>           
-                        <td class="texto-pequeno-empresa">Gmail: </td>
-                        <td><input class="" type="text" name="'.$v3.'" placeholder="exemplo@gmail.com"></td>
-                        <td class="texto-pequeno-empresa">CEP: </td>
-                        <td><input class="" type="text" name="'.$v4.'" placeholder="0000-000"></td>
-                    </tr>
-                    <tr>
-                        <td class="texto-pequeno-empresa">Telefone: </td>
-                        <td><input class="" type="text" name="'.$v5.'" placeholder="(00) 00000-0000 "></td>
-                    </tr>
-                </table>';
-                $numero += 1;
-                $v1 = $v1.'p';
-                $v2 = $v2.'p';
-                $v3 = $v3.'p';
-                $v4 = $v4.'p';
-                $v5 = $v5.'p';
-            }   
-        if(isset($_POST['cnpj' and 'nome_empresa' and 'nome_gerente' and 'gmail' and 'cep' and 'telefone'])){
-            echo 'oi'; 
-    $hostname = "127.0.0.1";
-    $name = "u935055604_wesley";
-    $password = "XwZX1383";
-    $DB = "u935055604_dados";
-
-            $conexao = new mysqli($hostname, $name, $password, $DB);//Tenta conexão com o DB
-
-            if ($conexao->connect_errno) {
-                echo "Failed conection: " . $conexao->connect_error; //erro caso não consiga conectar ao DB
-                exit();
-            } else {
-                $v1 = $_POST['cnpj']; //CNPJ
-                $v2 = $_POST['nome_empresa']; //Nome do gerente
-                $v3 = $_POST['gmail']; //Telefone
-                $v4 = $_POST['cep']; //CEP
-                $v5 = $_POST['telefone'];
-
-                $SQL = 'INSERT INTO `nossa_empresa_p` (`CNPJ_e`,`gmail_e`,`fone_e`,`CEP_e`,`nome_e`) 
-                VALUES (' . $v1 . ',' . $v2 . ',' . $v3 . ',' . $v4 . ',' . $v5 . ');';
-            //Inserir no DB
-                $resultado = $conexao->query($SQL);
-
-                $conexao->close();
-                header("Location: t_empresa_p.php"); //Envia para a tela de Login ao Cadastrar
-            }
-            echo '<table class="">';
-        }
-        echo '<input class="botao-empresa-submit" type="submit">';
-    }
-    }
-    echo'<div class=""></div>';
-    echo'</form>';
-?>
-
-            </form>
+        <div class="caixa-esquerda-turma">
+            <div class="texto-grande-turma">Crie seu Empresa</div>
+            <div class="caixa-esquerda-turma-dentro">
+                <form method="POST" action='t_empresa_p.php?cadastro=v&nome=<?php redirect()?>'>
+                    <div class="texto-cinza-turma">Empresa: </div>
+                    <input class="botao-turma-input" type="text" placeholder="nome" name="nome_e">
+                    <div class="texto-cinza-turma">CNPJ:</div>
+                    <input class="botao-turma-input" type="text" placeholder="000.000.000-00" name="cnpj">
+                    <div class="texto-cinza-turma">CEP: </div>
+                    <input class="botao-turma-input" type="text" placeholder="000.000.000-00" name="cep">
+                    <div class="texto-cinza-turma">Gmail:</div>
+                    <input class="botao-turma-input" type="text" placeholder="fornecedor@gmail.com" name="gmail">
+                    <div class="texto-cinza-turma">Telefone:</div>
+                    <input class="botao-turma-input" type="text" placeholder="(00) 00000-0000" name="telefone">
+                    <br>
+                    <input class="botao-turma-submit" type="submit" value="Cadastrar turma">
+                </form>
             </div>
-            </div>
-            <div class="caixa-direita-empresa">
-            <div class="texto-grande-empresa">Lista das Empresas</div>
-            <div class="caixa-direita-empresa-dentro">
-                <table class="tabela-empresa">
-                    <tr>
-                        <td class="linha-topo-2">A</td>
-                        <td class="linha-topo-2">B</td>
-                        <td class="linha-topo-2">C</td>
-                        <td class="linha-topo-2">D</td>
-                        <td class="linha-topo-2">E</td>
-                        <td class="linha-topo-2">F</td>
-                    </tr>
-                </table>
-            </div>
-            </div>
-    </div>
-    <div id="">Professor - <?php echo $nome;?></div>
+        </div>
+        <div class='conta-geral'>Professor - <?php echo $nome;?></div>
 </body>
 
 </html>
